92fed94544eceb015169bf6998ad013e8bca0249
marcofantasia <125381585+marcofantasia@users.noreply.github.com>
2023-02-19 11:43:13 +0100

    Add files via upload
    i miei esercizi in PHP di mercoledì 15 Febbraio 2023

---
+++ esercizi-mercoledì.php
@@ -0,0 +1,97 @@
+<?php
+
+// ESERCIZIO 1 
+
+// Dato un array di numeri,
+// scrivere un programma che mi permetta di avere la media SOLO dei numeri pari contenuti all’interno dell’array
+
+
+// $numbers = [1, 2, 4, 6, 9];
+
+// $sum = 0;
+
+// $count = 0;
+
+// foreach($numbers as $number){
+//     if($number % 2 == 0){
+//         $count++;
+//         $sum += $number;
+//     }
+// }
+
+// if($number > 0){
+//     $average = $sum / $count;
+//     echo "la media dei numeri pari è ${average}";
+// }else{
+//     echo "non ci sono numeri pari";
+// }
+
+
+
+
+            
+            
+            
+            
+            // ESERCIZIO 2
+            
+            // Dato un array di utenti con nome, cognome e genere, 
+            // per ogni utente stampare “Buongiorno Sig. Nome Cognome” o “Buongiorno Sig.ra Nome Cognome” a seconda del genere
+            
+            
+            // $users = array(
+            //     array('name' => 'Mario', 'surname' => 'Rossi', 'gender' => 'M'),
+            //     array('name' => 'Laura', 'surname' => 'Bianchi', 'gender' => 'F'),
+            //     array('name' => 'Luca', 'surname' => 'Verdi', 'gender' => 'M'),
+            //     array('name' => 'Anna', 'surname' => 'Neri', 'gender' => 'F')
+            // );
+            
+
+
+            // foreach($users as $user){
+            //     if ($user['gender'] == 'M'){
+            //         $salutation = 'Buongiorno Sig.';
+            //     } elseif ($user['gender'] == 'F'){
+            //         $salutation = 'Buongiorno Sig.ra';
+            //     } else{
+            //         $salutation = 'Buongiorno';
+            //     }
+                
+            //     echo $salutation . $user['name'] . ' ' . $user['surname'] . '/n';
+            // }
+
+            
+
+
+            // ESERCIZIO 3
+
+
+// - Scrivere un programma che stampi in console tutti i numeri da uno a cento. 
+// Se il numero è multiplo di 3, stampare “PHP” al posto del numero;
+//  se multiplo di 5 deve stampare “JAVASCRIPT”; 
+// se multiplo di 3 e 5 (15) deve stampare “HACKADEMY66".
+
+
+// for($i = 0; $i <= 100; $i++){
+// if($i % 3 == 0){
+//         echo "PHP";
+//     } else if($i % 5 == 0){
+//         echo "JAVASCRIPT";
+//     } else if($i % 3 == 0 && $i % 5 == 0){
+//         echo "HACKADEMY66";
+//     } else{
+//         echo $i . "/n";
+//     }
+
+
+
+    
+// }
+
+
+
+
+
+            
+            
+            
